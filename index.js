0f4feb3cea121daa0b10c56f07d7380c305c1ae9
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-02-12 22:52:32 -0600

    Switched back to using XPATH and got it to work

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const PORT = 8000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const xpath = require('xpath');
-dom = require('xmldom').DOMParser;
+var xpath = require('xpath')
+dom = require('xmldom').DOMParser
+
+// Xpaths
+const timePlayedXPATH = "//div[@class='progress-category is-partial toggle-display is-active']//div[@class='ProgressBar-title' and contains(text(), '%s')]/..//div[@class='ProgressBar-description']"
 
 // Initionalization
 const app = express();
@@ -14,18 +17,34 @@ app.get('/', (req, res) => {
     res.json("Welcome to my api");
 })
 
-app.get('/news', (req, res) => {
-    const overwatchURL = 'https://playoverwatch.com/en-us/career/pc/Jflyer45-1172/';
+// app.get('/getStat', (req, res) => {
+//     const overwatchURL = 'https://playoverwatch.com/en-us/career/pc/Jflyer45-1172/';
+//     axios.get(overwatchURL)
+//     .then((response) => {
+//         const html = response.data;
+//         const $ = cheerio.load(html)
+//         let bar = $('div[ProgressBar-title]:contains("Mercy")').text();
+        
+//         console.log("Thingy: " + bar)
+        
+
+//     }).catch((err) => console.log(err))
+// })
+app.get('/getStat/:username/:hero', (req, res) => {
+    let username = req.params.username
+    const overwatchURL = 'https://playoverwatch.com/en-us/career/pc/%s-1172/'.replace("%s", username);
+    let chosenHero = req.params.hero
     axios.get(overwatchURL)
     .then((response) => {
-        const html = response.data;
-        doc = new dom().parseFromString(html);
-        const times = xpath.select("//div[@class='ProgressBar-description']", html);
-        console.log(times);
-        // const $ = cheerio.load(html)
 
-        // $('a:')
-    })
+        const html = response.data;
+        var doc = new dom().parseFromString(html)
+        var nodes = xpath.select(timePlayedXPATH.replace("%s", chosenHero), doc)
+        let timePlayed = nodes[0].firstChild.data
+        res.json(timePlayed)
+
+    }).catch((err) => console.log(err))
 })
+
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT))
