f366d57102d8ac3233b0ee03554c90c52042498d
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-02-13 10:30:52 -0600

    Removed unneeded comments

--- index.js
+++ index.js
@@ -17,19 +17,6 @@ app.get('/', (req, res) => {
     res.json("Welcome to my api");
 })
 
-// app.get('/getStat', (req, res) => {
-//     const overwatchURL = 'https://playoverwatch.com/en-us/career/pc/Jflyer45-1172/';
-//     axios.get(overwatchURL)
-//     .then((response) => {
-//         const html = response.data;
-//         const $ = cheerio.load(html)
-//         let bar = $('div[ProgressBar-title]:contains("Mercy")').text();
-        
-//         console.log("Thingy: " + bar)
-        
-
-//     }).catch((err) => console.log(err))
-// })
 app.get('/getStat/:username/:hero', (req, res) => {
     let username = req.params.username
     const overwatchURL = 'https://playoverwatch.com/en-us/career/pc/%s-1172/'.replace("%s", username);
